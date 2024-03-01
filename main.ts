7d4bc96021f678e8659336b962516c0cde0a63ba
wse0odero8ghj79wq9h8e7wq8ew <160520265+wse0odero8ghj79wq9h8e7wq8ew@users.noreply.github.com>
2024-03-01 13:29:15 -0700

    very cool extension

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+namespace easy {
+    export function print(str : string): void {
+        basic.showString(str)
+    }
+    export function wait(time : number): void {
+        basic.pause(time*1000)
+    }
+    export function clear(): void {
+        basic.clearScreen()
+    }
+}
