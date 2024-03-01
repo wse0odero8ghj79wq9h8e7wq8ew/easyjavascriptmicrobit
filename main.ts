0e0da367eee15deb160bb52f3f14a692521243b5
wse0odero8ghj79wq9h8e7wq8ew <160520265+wse0odero8ghj79wq9h8e7wq8ew@users.noreply.github.com>
2024-03-01 13:40:20 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
 namespace easy {
     /**
-     * Thingy
+     * Prints whatever is inside of it on the microbit.
      */
     //% blockId=easyprint
     //% block="print $str"
     export function print(str : string): void {
         basic.showString(str)
     }
+    /**
+     * Waits for an amount of time in seconds.
+     */
+    //% blockId=easywait
+    //% block="wait $time"
     export function wait(time : number): void {
         basic.pause(time*1000)
     }
+    /**
+     * Clears the screen.
+     */
+    //% blockId=easyclear
+    //% block="clear"
     export function clear(): void {
         basic.clearScreen()
     }
