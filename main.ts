c08dfc0cc0d90b8ab9284ae393635cea7383435a
wse0odero8ghj79wq9h8e7wq8ew <160520265+wse0odero8ghj79wq9h8e7wq8ew@users.noreply.github.com>
2024-03-04 16:09:30 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,7 +34,50 @@ namespace easyScript {
            let _health = 100
            canaddhealthvar = false
         } else {
-            let _health = 100
+            let _health2 = 100
         }
     }
+    /**
+     * Lights a random tile.
+     */
+    //% blockId="specialrandomtile"
+    //% block="light random tile"
+    export function randomtile(): void {
+        let randx = randint(0, 4)
+        let randy = randint(0, 4)
+        led.plot(randx, randy)
+    }
+    /**
+     * Lights a random tile with random transparency.
+     */
+    //% blockId="specialrandomtiletransparency"
+    //% block="light random transparency tile"
+    export function randomtiletransparency(): void {
+        let randx2 = randint(0, 4)
+        let randy2 = randint(0, 4)
+        let randbright = randint(0, 255)
+        led.plotBrightness(randx2, randy2, randbright)
+    }
+    /**
+     * Prints a factorial.
+     */
+    //% blockId="specialfactorial"
+    //% block="factorial $num $factorialnumber"
+    export function factorial(num: number) {
+        let multiply = num
+        let answer = 0
+        if (num <= -1) {
+            return 0;
+        }
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        answer = num
+        while (multiply >= 3) {
+            multiply = multiply - 1
+            answer = answer * multiply
+        }
+        return answer;
+    }
+
 }
