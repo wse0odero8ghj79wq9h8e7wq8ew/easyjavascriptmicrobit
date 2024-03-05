83b8e186d43e18a4b872eb0b008f116f425f4969
wse0odero8ghj79wq9h8e7wq8ew <160520265+wse0odero8ghj79wq9h8e7wq8ew@users.noreply.github.com>
2024-03-04 19:33:10 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -101,7 +101,7 @@ namespace led {
     }
 }
 
-namespace extras {
+namespace extraled {
     /**
      * Lights a random tile.
      */
@@ -200,32 +200,5 @@ namespace extras {
         }
         grabindexbycoords(numx,numy)
         return gibc;
-    }
-}
-
-namespace easyScript {
-    /**
-     * Prints whatever is inside of it on the microbit.
-     */
-    //% blockId=easyprint
-    //% block="print $str"
-    export function print(str : string): void {
-        basic.showString(str)
-    }
-    /**
-     * Waits for an amount of time in seconds.
-     */
-    //% blockId=easywait
-    //% block="wait $time"
-    export function wait(time : number): void {
-        basic.pause(time*1000)
-    }
-    /**
-     * Clears the screen.
-     */
-    //% blockId=easyclear
-    //% block="clear"
-    export function clear(): void {
-        basic.clearScreen()
     }
 }
